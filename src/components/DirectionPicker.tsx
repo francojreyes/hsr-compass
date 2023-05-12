b86b5e4484742c66a546e927c00ee5f68e1c89cb
Franco Reyes <franco.javier.reyes@gmail.com>
2023-05-13 02:16:18 +1000

    Created picker for rotation direction

---
+++ src/components/DirectionPicker.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Radio, RadioGroup } from '@mui/joy';
+
+const DirectionPicker: React.FC = () => {
+  const [value, setValue] = React.useState(1);
+  console.log(value);
+  return (
+    <RadioGroup
+      size='md'
+      value={value}
+      onChange={e => setValue(parseInt(e.target.value))}
+    >
+      <Radio value='1' label='Clockwise' variant='solid'/>
+      <Radio value='-1' label='Counter Clockwise' variant='solid'/>
+    </RadioGroup>
+  )
+}
+
+export default DirectionPicker;
